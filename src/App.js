3a9fdc7d2914a6a07317b02a3b3bcdccdde28fc0
Muh. Dirga F <muh.dirga.f@gmail.com>
2021-11-09 10:10:25 +0800

    make it better

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 import Meet from "./Meet";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function MainMenu() {
   return (
